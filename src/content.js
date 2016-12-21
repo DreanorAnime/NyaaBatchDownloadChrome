0fb363bd3b7dde091c21f55e10d0610936e79229
Mike <mikeundead@googlemail.com>
2016-12-21 04:27:35 +0100

    Update content.js

--- src/content.js
+++ src/content.js
@@ -5,9 +5,9 @@ document.getElementById("searchcontainer").appendChild(button);
 
 function download() 
 {
-	var x = document.getElementsByClassName("tlistdownload");
-	for(i = 0; i < x.length; i++)
-	{
-		window.open(x[i].getElementsByTagName('a')[0].href);
-	}
+    var x = document.getElementsByClassName("tlistdownload");
+    for(i = 0; i < x.length; i++)
+    {
+        window.open(x[i].getElementsByTagName('a')[0].href);
+    }
 }
