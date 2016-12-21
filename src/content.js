7fe11a55a9b1090d6d7e0ab1fc79bd68d3bf44bd
Mike <mikeundead@googlemail.com>
2016-12-21 04:15:00 +0100

    Add files via upload

---
+++ src/content.js
@@ -0,0 +1,13 @@
+var button = document.createElement('button');
+button.innerHTML = 'Download current results';
+button.addEventListener('click', function() { download()});    
+document.getElementById("searchcontainer").appendChild(button);
+
+function download() 
+{
+	var x = document.getElementsByClassName("tlistdownload");
+	for(i = 0; i < x.length; i++)
+	{
+		window.open(x[i].getElementsByTagName('a')[0].href);
+	}
+}
